02ebffe38ac5e644508c6ef7c5470fc666165c1a
cristian.cirdei01@e-uvt.ro <83004817+cris-alt@users.noreply.github.com>
2022-11-03 10:51:00 +0200

    original commit

--- src/components/molecules/Menu.jsx
+++ src/components/molecules/Menu.jsx
@@ -4,6 +4,7 @@ import MenuButton from "../atoms/MenuButton";
 const Menu = () => {
   return (
     <div className="col-2 menu">
+      <h1 className="dash-title">Teacher Dashboard</h1>
       <MenuButton link="/classes" page="Classes"></MenuButton>
       <MenuButton link="/students" page="Students"></MenuButton>
       <MenuButton link="/add/grades" page="Grades"></MenuButton>
